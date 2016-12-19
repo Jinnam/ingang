2688f986dee91801b4a9d7eac4b0d0cd7277a205
Administrator <Administrator@smart20102>
2016-12-19 12:23:14 +0900

    Jinnam commit
    Member클래스와 MemberDao클래스 생성(내용 없음)

---
+++ Ingang/src/kr/co/no1/member/controller/MemberInsertAction.java
@@ -0,0 +1,21 @@
+package kr.co.no1.member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import inter.Action;
+import inter.ActionForward;
+
+public class MemberInsertAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		request.setCharacterEncoding("euc-kr");
+		
+		
+		
+		return null;
+	}
+
+}
---
+++ Ingang/src/kr/co/no1/member/db/MemberDao.java
@@ -0,0 +1,5 @@
+package kr.co.no1.member.db;
+
+public class MemberDao {
+
+}
